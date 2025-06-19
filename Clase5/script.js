263d27214104ebc32ef06626eee08c4c9569aac3
ElisaCarstairs <marina.arredondo.h@gmail.com>
2025-06-18 20:24:55 -0600

    Añadir formulario de fiesta

--- Clase5/script.js
+++ Clase5/script.js
@@ -3,9 +3,11 @@ const form = document.getElementById('myForm');
 form.addEventListener('submit', function(event) {
   event.preventDefault(); // Previene el envío automático del formulario
   const name = document.getElementById('name').value;
+  const lastname = document.getElementById('lastname').value;
   const email = document.getElementById('email').value;
   const telefono = document.getElementById('telefono').value;
-  
+  const intereses = document.querySelectorAll('input[name="intereses"]:checked');
+  const horario = document.querySelector('input[name="horario"]:checked');
 
   if (!email.includes('@')) {
     alert('Por favor, introduce un correo electrónico válido.');
@@ -15,6 +17,27 @@ form.addEventListener('submit', function(event) {
     alert('Por favor, introduce un teléfono de 10 dígitos.');
     return;
   }
-  console.log(`Nombre: ${name}, Correo: ${email}, Teléfono: ${telefono}`);
+  if (horario === null) {
+    alert('Por favor, selecciona un horario.');
+    return;
+  }
+  if (name.trim() === '') {
+  alert('Por favor, ingrese su Nombre.');
+  return;
+}
+
   
+  const salida = document.getElementById('salida');
+
+    salida.innerHTML = `
+    <strong>Datos capturados:</strong><br>
+    Nombre: ${name}<br>
+    Apellido: ${lastname}<br>
+    Correo: ${email}<br>
+    Teléfono: ${telefono}<br>
+    Intereses: ${Array.from(intereses).map(i => i.value).join(", ")}<br>
+    Horario: ${horario.value}
+`;
+    alert(`${name}, ha sido registrado correctamente.`);
+    form.reset();
 });
